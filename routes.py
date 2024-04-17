8eb13e57f1c5724ded47e29ad3f41293aa9e1f41
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-17 19:04:01 +0100

    upd

--- routes.py
+++ routes.py
@@ -1,16 +1,23 @@
 from fastapi import APIRouter, status
 from controllers import Controllers
-from schemas import PersonResponseSchema
 from typing import List
 
 router = APIRouter()
 
 
-router.get("/",
-           status_code=status.HTTP_200_OK
-           )(Controllers.root)
+router.get(
+        "/",
+        status_code=status.HTTP_200_OK
+        )(Controllers.root)
 
 
-router.get("/bdays/",
-           status_code=status.HTTP_200_OK,
-           )(Controllers.list_birthdays)
+router.get(
+        "/bdays/",
+        status_code=status.HTTP_200_OK,
+        )(Controllers.list_birthdays)
+
+
+router.post(
+        "/bday/new",
+        status_code=status.HTTP_201_CREATED
+        )(Controllers.add_birthday)
