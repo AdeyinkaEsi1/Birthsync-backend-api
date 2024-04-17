24a7cf5627eb41f0af7d73831de3cff1605766ae
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-17 19:02:42 +0100

    upd

--- controllers.py
+++ controllers.py
@@ -1,6 +1,8 @@
-from models import Person
-from schemas import PersonResponseSchema
+from fastapi import HTTPException
+from models import *
+from schemas import *
 from typing import List
+from mongoengine import NotUniqueError
 
 class Controllers:
     
@@ -14,7 +16,16 @@ class Controllers:
         for bd in data:
             bday_data.append(
                 {"name": bd.name,
-                 "birthday": bd.birth_date,
+                 "birth_date": bd.birth_date,
                  "extra_info": bd.extra_info}
             )
         return bday_data
+    
+    def add_birthday(data: PersonCreateSchema):
+        try:
+            new_data = Person(name=data.name, birth_date=data.birth_date, extra_info=data.extra_info)
+            new_data.save()
+            return {"message": "Data created successfully"}
+        except NotUniqueError:
+            raise HTTPException(status_code=406, detail="data not unique")
+        
