9b848d2b1250e9d63ebb42236ae03f845d459022
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-17 14:41:17 +0100

    updated

--- schemas.py
+++ schemas.py
@@ -4,5 +4,9 @@ from datetime import date
 
 class PersonBaseSchema(BaseModel):
     name: str = ...
-    birth_date: date = Field(..., description="Date of birth", gt=date(1950, 1, 1))
+    birth_date: date = Field(..., description="Date of birth")
     extra_info: str = None
+    
+
+class PersonResponseSchema(PersonBaseSchema):
+    pass
