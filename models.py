943b1570c00784de9fe8b279ae806b9af04e7a74
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-17 19:03:22 +0100

    upd

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from mongoengine import Document, StringField, DateField
 
 class Person(Document):
-    name = StringField(max_length=50)
-    birth_date = DateField
-    extra_info = StringField(max_length=200, required=False)
+    name = StringField(max_length=50, unique=True, required=True)
+    birth_date = DateField(required=True)
+    extra_info = StringField(max_length=200, required=True)
