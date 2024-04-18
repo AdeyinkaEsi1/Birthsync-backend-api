e8c256618eb3811b0282ab57e5540435ec1f589d
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-18 09:42:16 +0100

    updated

--- routes.py
+++ routes.py
@@ -13,7 +13,7 @@ router.get(
 
 router.get(
         "/bdays/",
-        status_code=status.HTTP_200_OK,
+        status_code=status.HTTP_200_OK
         )(Controllers.list_birthdays)
 
 
@@ -21,3 +21,21 @@ router.post(
         "/bday/new",
         status_code=status.HTTP_201_CREATED
         )(Controllers.add_birthday)
+
+
+router.put(
+        "/bday/update",
+        status_code=status.HTTP_202_ACCEPTED
+        )(Controllers.update_birthday)
+
+
+router.get(
+        "/bday/get",
+        status_code=status.HTTP_200_OK
+        )(Controllers.get_birthday)
+
+
+router.delete(
+        "/bday/get",
+        status_code=status.HTTP_204_NO_CONTENT
+        )(Controllers.delete_birthday)
