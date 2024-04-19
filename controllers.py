098d421403ebd25ea0bf84bb2fef1ccb4c51b89b
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-19 18:59:45 +0100

    upd

--- controllers.py
+++ controllers.py
@@ -2,8 +2,13 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from models import *
 from schemas import *
-from typing import Annotated, List
+from typing import Annotated, List, Union
 from mongoengine import NotUniqueError, DoesNotExist
+from datetime import datetime, timedelta, timezone
+from pydantic import BaseModel
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -13,99 +18,9 @@ class Controllers:
         return {"token": token}
         # return {"message": "ROOT ENDPOINT"}
 
-
-    fake_users_db = {
-        "johndoe": {
-            "username": "johndoe",
-            "full_name": "John Doe",
-            "email": "johndoe@example.com",
-            "hashed_password": "fakehashedsecret",
-            "disabled": False,
-        },
-        "alice": {
-            "username": "alice",
-            "full_name": "Alice Wonderson",
-            "email": "alice@example.com",
-            "hashed_password": "fakehashedsecret2",
-            "disabled": True,
-        },
-    }
-
-    def fake_hash_password(password: str):
-        return "fakehashed" + password
-
-    class User(BaseModel):
-        username: str
-        email: Union[str, None] = None
-        full_name: Union[str, None] = None
-        disabled: Union[bool, None] = None
-
-
-    class UserInDB(User):
-        hashed_password: str
-
-
-    def get_user(db, username: str):
-        if username in db:
-            user_dict = db[username]
-            return Controllers.UserInDB(**user_dict)
-
-
-    def fake_decode_token(token):
-        # This doesn't provide any security at all
-        # Check the next version
-        user = Controllers.get_user(Controllers.fake_users_db, token)
-        return user
-
-
-    async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-        user = Controllers.fake_decode_token(token)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        return user
-
-
-    async def get_current_active_user(
-        current_user: Annotated[User, Depends(get_current_user)],
-    ):
-        if current_user.disabled:
-            raise HTTPException(status_code=400, detail="Inactive user")
-        return current_user
-
-
-    async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-        user_dict = Controllers.fake_users_db.get(form_data.username)
-        if not user_dict:
-            raise HTTPException(status_code=400, detail="Incorrect username or password")
-        user = Controllers.UserInDB(**user_dict)
-        hashed_password = Controllers.fake_hash_password(form_data.password)
-        if not hashed_password == user.hashed_password:
-            raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-        return {"access_token": user.username, "token_type": "bearer"}
-
-
-    async def read_users_me(
-        current_user: Annotated[User, Depends(get_current_active_user)],
-    ):
-        return current_user
-
-
-
-
-
-
-
-
-
-
-
-
-
+    SECRET_KEY = "aa78aca0d83af707dcac482ce4226d14ae75d80a4ba8656705f7543ea00e55f4"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
     def list_birthdays() -> List[PersonResponseSchema]:
         data = Person.objects.all()
