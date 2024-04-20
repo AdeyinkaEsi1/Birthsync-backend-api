ba380aa6644cea3c7bac63c14f769ff0c8129b42
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-20 23:04:35 +0100

    fix - formatted

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
-from mongoengine import Document, StringField, DateField
+from mongoengine import( 
+    Document, 
+    StringField, 
+    DateField)
 
 class Person(Document):
     name = StringField(max_length=50, unique=True, required=True)
