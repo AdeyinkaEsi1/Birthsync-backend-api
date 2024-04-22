74b71c8bd4c56cbe341f2773fb5a4c2c327f9aa4
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-22 23:33:01 +0100

    upd

--- models.py
+++ models.py
@@ -9,3 +9,9 @@ class Person(Document):
     name = StringField(max_length=50, unique=True, required=True)
     birth_date = DateField(required=True)
     extra_info = StringField(max_length=200, required=True)
+
+
+class BaseAccount(Document):
+    username: str
+    password: str
+
