60aedd81c7f64e76e22c24d19fd046f9c18aaebe
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-05-27 12:18:29 +0100

    upd -- cors

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from mongoengine import connect
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException
 from routes import router
 from apscheduler.jobstores.mongodb import MongoDBJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 
 connect("bdsync")
@@ -15,3 +18,13 @@ router = APIRouter()
 
 jobstore = MongoDBJobStore(database="bdsync", collection="jobs")
 scheduler = AsyncIOScheduler(jobstores={"mongo": jobstore})
+
+
+# Allow CORS for local development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # specific origins in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
