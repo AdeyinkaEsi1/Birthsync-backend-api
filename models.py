946ecab15c6394736efc9d8c1f206d40122356eb
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-06-20 22:32:53 +0100

    fixed

--- models.py
+++ models.py
@@ -1,14 +1,17 @@
+import datetime
 from mongoengine import( 
     Document, 
     StringField, 
+    DateTimeField,
     DateField)
 
-
+    
 class Person(Document):
     name = StringField(max_length=50, unique=True, required=True)
     birth_date = DateField(required=True)
     extra_info = StringField(max_length=200, required=True)
-
+    # time = DateTimeField(default=datetime.datetime.utcnow())
+    
 
 class BaseAccount(Document):
     username = StringField(max_length=50)
